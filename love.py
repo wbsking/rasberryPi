4aae97a93604aa89e3bebe3848b9a2c5653c258b
sking <wbsking@gmail.com>
2015-01-04 23:47:10 +0800

    feat(love):修改初始化错误

--- love.py
+++ love.py
@@ -12,7 +12,7 @@ class Love(object):
     def run(self):
         for i in xrange(16, 0, -1):
             for j in xrange(1, 9):
-                self.light.light_on(Point(i, j))
+                self.light.light_off(Point(i, j))
         self.light.light_on(Point(8, 1))
         bcm.delay(80)
         self.light.light_on(Point(9, 1))
